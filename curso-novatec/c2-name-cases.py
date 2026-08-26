8bc928f6e5d4078029a27f9c50380765d89ee874
Isabella ELoy <isabellaeloy.be@gmail.com>
2026-08-26 11:25:37 -0300

    exercices c2 and c3

---
+++ curso-novatec/c2-name-cases.py
@@ -0,0 +1,33 @@
+
+#2.3
+message = "Oi Bella, você gostaria de aprender python hoje?"
+print(message)
+
+#2.4
+name = "bella"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+
+#2.5
+print("Carmen Herrera once said 'You don't have to talk about art, you have to art about art'")
+
+#2.6
+author = "Carmen Herrera"
+quote = "'You don't have to talk about art, you have to art about art'"
+
+print(f"{author} once said {quote}")
+
+#2.7
+famous_people = "  Robert Pattinson "
+print(famous_people)
+
+print(famous_people.lstrip())
+print(famous_people.rstrip())
+print(famous_people.strip())
+
+#2.8
+filename = "python_notes.txt"
+print(filename.removesuffix('.txt'))
+
